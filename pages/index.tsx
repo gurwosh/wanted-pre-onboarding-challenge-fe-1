cd120f444ee306afd531ac0a654055f664e4188a
gurwosh <fkauswogur@naver.com>
2023-01-08 20:14:11 +0900

    "dev 1"

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,22 @@ import Login from "../Auth/login";
 
 export default function Home() {
 
-  const[todos,Settodos] =useState()
   const[Detail, Setdetail] =useState({'title':'','contents':''})
+  const[tabledata,setTable] = useState([]);
+  const[Detailtitle,Settitle] = useState< String | null>(null);
+  const[Detailcontent,Setcontent] = useState< String | null>(null);
+
 
   useEffect(()=>{
+    let k = localStorage.getItem('title') ? localStorage.getItem('title'): '';
+    let w = localStorage.getItem('content') ? localStorage.getItem('content'): '';
+
+    Settitle(k)
+    Setcontent(w)
+    
     if(localStorage.getItem('login')){
       api.fetchTodos(localStorage.getItem('login')).then((data)=>{
-        console.log(data)
+          setTable(data['data'])
       })
     }
   },[])
@@ -41,21 +50,43 @@ export default function Home() {
     console.log(Detail)
   }
 
-  //senddat api를 호출하는 부분
+  const displaytodo = () =>{
+
+    let result = []
+   for(let i in tabledata){
+    result.push(<div onClick={viewdetail} key={i} id={i} ><p>title : {tabledata[i]['title']}</p></div>)
+   }
+
+    return result
+  }
 
   const senddata =()=>{
      api.fetchCreateTodos(Detail,localStorage.getItem('login')).then((data)=>
-     console.log(data) 
+        location.reload()
      )
     
   }
 
 
+  const viewdetail = (e: any) => {
+    const pid = e.currentTarget.id.toString()
+    let k = tabledata[pid]['title']
+    let w = tabledata[pid]['content']
 
+    localStorage.setItem('title', k)
+    localStorage.setItem('content', w)
 
+    Settitle(k)
+    Setcontent(w)
+  }
 
 
   const Detailtodo =()=>{
+    return (<div>{Detailtitle}+{Detailcontent}</div>)
+  }
+
+
+  const Createtodo =()=>{
 
     return(
     <>
@@ -76,7 +107,8 @@ export default function Home() {
     <button onClick={gosignin}>회원가입페이지로</button>
     </div>
     <div>
-    {}
+    {displaytodo()}
+    {Createtodo()}
     </div>
     <div>
     {Detailtodo()}
