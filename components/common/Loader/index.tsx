b74398496e006590df4428481dd17c9ef90d2863
gurwosh <fkauswogur@naver.com>
2023-01-18 21:39:27 +0900

    "common 폴더 작업"

---
+++ components/common/Loader/index.tsx
@@ -0,0 +1,31 @@
+import {
+    LoaderTodoItem,
+    LoaderTodoList,
+    LoaderTodoModalContent,
+    LoaderTodoModalCreatedAt,
+    LoaderTodoModalTitle,
+    LoaderTodoModalWrap,
+  } from "./style";
+  
+  export const TodosFallbackLoader = () => (
+    <LoaderTodoList>
+      <LoaderTodoItem />
+      <LoaderTodoItem />
+      <LoaderTodoItem />
+      <LoaderTodoItem />
+      <LoaderTodoItem />
+      <LoaderTodoItem />
+      <LoaderTodoItem />
+    </LoaderTodoList>
+  );
+  
+  export const TodoModalFallbackLoader = () => (
+    <LoaderTodoModalWrap>
+      <LoaderTodoModalTitle />
+      <LoaderTodoModalContent />
+      <LoaderTodoModalContent />
+      <LoaderTodoModalContent />
+      <LoaderTodoModalCreatedAt />
+    </LoaderTodoModalWrap>
+  );
+  
